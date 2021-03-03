6a9b9f878b250723ce61f40f98fba3683505278d
CrowBaldeYT <75171427+CrowBaldeYT@users.noreply.github.com>
2021-03-03 12:31:19 +0100

    added AfkTimer

---
+++ src/DXM_Hip/AfkKick/task/afkKick.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DXM_Hip\AfkKick\task;
+
+use pocketmine\scheduler\Task;
+use DXM_Hip\AfkKick\Main;
+
+class afkTimer extends Task {
+    
+    public function __construct(Main $plugin) {
+         $this->plugin = $plugin;
+    }
+    
+    public function onRun($currentTick) 
+    {
+        foreach($this->plugin->getServer()->getOnlinePlayers() as $player){
+           if(!isset($this->plugin->time[$player->getName()]))
+           {
+           $this->plugin->setTime($player);    
+           }
+           
+           if(!isset($this->plugin->pos[$player->getName()]))
+           {
+           $this->plugin->setPos($player);
+           }
+           
+           if(!$this->plugin->hasMoved($player)){
+           $this->plugin->removePlayer($player);
+           
+           }elseif(!$this->plugin->isPlayerSet($player))
+           {
+           $this->plugin->setPlayer($player);
+           }
+        }
+        $this->plugin->checkTime();
+        
+    }
+}
